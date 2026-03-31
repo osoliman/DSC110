7856e21bfaaadbfcb1eae359239d26b4c6956b64
Omar Soliman <31664030+osoliman@users.noreply.github.com>
2026-03-30 22:01:38 -0400

    Create Intro_to_R.R with basic R examples
    Added basic R script for printing, calculations, and reading a CSV file.

---
+++ Causal_Inference/Intro_to_R.R
@@ -0,0 +1,16 @@
+#Print Hello World
+print("Hello World")
+
+#Basic mathematical calculations
+1+1
+
+#Variables
+a <- 1
+b <- 2 
+a + b
+
+#Read csv file
+df <- read.csv('https://raw.githubusercontent.com/osoliman/DSC110/refs/heads/main/Datasets/nhanes_final.csv')
+head(df)
+
+head(df$age)
